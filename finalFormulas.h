cfa20c33c0fb77e3c154833890bd29daf168c928
austenrud <48618743+austenrud@users.noreply.github.com>
2019-04-24 13:23:40 +0200

    Create finalFormulas.h

---
+++ finalFormulas.h
@@ -0,0 +1,53 @@
+//
+// Created by Austen on 24/04/2019.
+//
+
+#ifndef CASECOMBINE_FINALFORMULAS_H
+#define CASECOMBINE_FINALFORMULAS_H
+
+// formulas for Binary Search
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+double installment, PV, ftemp;
+double f(double installment,double PV,double periodicInterestRate, double n){
+    double temp;
+    temp=periodicInterestRate * (1 + (1 / ((pow(1 + periodicInterestRate, n)) - 1)));
+    temp-= installment / PV;
+    return temp;
+
+}
+
+//Binary Search
+double periodicInterestRate;
+int n;
+
+double binarySearch(double lBound, double uBound)
+{
+    do {
+        periodicInterestRate = lBound + (uBound - lBound)/2;
+        ftemp = f(installment,PV,periodicInterestRate,n);
+
+        if (ftemp > 0)
+        {
+            uBound = periodicInterestRate;
+        }
+        else
+        {
+            lBound = periodicInterestRate;
+        }
+
+    } while (fabs(uBound-lBound) > 0.00000000000001); // need fabs here, not abs (abs only for integers)
+
+    return periodicInterestRate;
+}
+
+ // Enter the CSV code here
+
+
+
+#endif //CASECOMBINE_FINALFORMULAS_H
