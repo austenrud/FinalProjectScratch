960f8ca0c34e1a0deaa48767a0e6a1f2ef8f8a56
austenrud <48618743+austenrud@users.noreply.github.com>
2019-04-25 13:29:10 +0200

    Update finalFormulas.h

--- finalFormulas.h
+++ finalFormulas.h
@@ -5,11 +5,15 @@
 #ifndef CASECOMBINE_FINALFORMULAS_H
 #define CASECOMBINE_FINALFORMULAS_H
 
-// formulas for Binary Search
-
+// formulas for Binary Search and Box Muller
 #include <iostream>
-#include <cmath>
+#include<cmath>
 #include <fstream>
+#include <chrono>
+#include <random>
+#include<cstdlib>
+#include <ctime>
+
 
 using namespace std;
 
@@ -21,6 +25,8 @@ double f(double installment,double PV,double periodicInterestRate, double n){
     return temp;
 
 }
+
+
 
 //Binary Search
 double periodicInterestRate;
@@ -46,8 +52,108 @@ double binarySearch(double lBound, double uBound)
     return periodicInterestRate;
 }
 
+// BoxMuller Code in BoxMullerCase2.h
+double boxMuller(double pmean, double pvar){
+
+    double x, y, s, z, p;
+
+    do {
+        x = ((double) rand() * 2 / RAND_MAX) - 1;
+        y = ((double) rand() * 2 / RAND_MAX) - 1;
+        s = x * x + y * y;
+    } while (s > 1);
+
+    z = x * sqrt(-2 * log(s) / s);
+    p = sqrt(pvar) * z + pmean;
+
+    return p;
+}
+
+
  // Enter the CSV code here
+ int variableChoice;
+
+char listSeparator=',';
+ofstream csvFile;
+// The below works as a function***
+int openingMonth;
+double r;
+void csvFixed(int tempMonth, int tempYear, int frequency, double openingBalance, double principal, double periodicInterestRate, double interest, double installment, double closingBalance)
+{
+
+    csvFile<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<endl;
+    csvFile<<"input"<<listSeparator<<"PV"<<listSeparator<<PV<<endl;
+    csvFile<<""<<listSeparator<<"frequency"<<listSeparator<<frequency<<endl;
+    csvFile<<""<<listSeparator<<"# of periods"<<listSeparator<<n<<endl;
+    csvFile<<""<<listSeparator<<"interest rate"<<listSeparator<<r<<endl;
+    csvFile<<""<<listSeparator<<"start month"<<listSeparator<<openingMonth<<endl;
+    csvFile<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<endl;
 
 
+    csvFile<<"Period Number"<<listSeparator<<"Date"<<listSeparator<<"Opening Balance"<<listSeparator<<"Principal"<<listSeparator<<"r"<<listSeparator<<"Interest"<<listSeparator<<"Installments"<<listSeparator<<"Closing Balance"<<endl;
+
+
+    for (int i = 1; i <= n; i++)
+    {
+        csvFile<<i<<listSeparator<<tempMonth<<"/"<<tempYear<<listSeparator<<openingBalance<<listSeparator<<principal<<listSeparator<<periodicInterestRate<<listSeparator<<interest<<listSeparator<<installment<<listSeparator<<closingBalance<<endl;
+        // temporary month value to be recalculated after each iteration
+        // at each iteration increase month value by frequency
+        tempMonth += frequency;
+        // if the end of year
+        if (tempMonth > 12)
+        {
+            // reset month value to january
+            tempMonth = (tempMonth-12);
+            // increase year
+            tempYear++;
+        }
+        openingBalance = closingBalance;
+        interest = openingBalance*periodicInterestRate;
+        principal=installment-interest;
+
+        closingBalance = openingBalance-principal;
+    }
+}
+double p, desiredMean, desiredVar, margin, floatRateMean, floatRateStd;
+void csvFloat(int tempMonth, int tempYear, int frequency, double openingBalance, double principal, double periodicInterestRate, double interest, double installment, double closingBalance)
+{
+    csvFile<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<endl;
+    csvFile<<"input"<<listSeparator<<"PV"<<listSeparator<<PV<<endl;
+    csvFile<<""<<listSeparator<<"frequency"<<listSeparator<<frequency<<endl;
+    csvFile<<""<<listSeparator<<"# of periods"<<listSeparator<<n<<endl;
+    csvFile<<""<<listSeparator<<"margin"<<listSeparator<<margin<<endl;
+    csvFile<<""<<listSeparator<<"floating rate mean"<<listSeparator<<floatRateMean<<endl;
+    csvFile<<""<<listSeparator<<"floating rate st dev"<<listSeparator<<floatRateStd<<endl;
+    csvFile<<""<<listSeparator<<"start month"<<listSeparator<<openingMonth<<endl;
+    csvFile<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<listSeparator<<""<<endl;
+
+    for (int i = 1; i <= n; i++)
+    {
+        csvFile<<i<<listSeparator<<tempMonth<<"/"<<tempYear<<listSeparator<<openingBalance<<listSeparator<<principal<<listSeparator<<periodicInterestRate<<listSeparator<<interest<<listSeparator<<installment<<listSeparator<<closingBalance<<endl;
+        // temporary month value to be recalculated after each iteration
+        // at each iteration increase month value by frequency
+        tempMonth += frequency;
+        // if the end of year
+        if (tempMonth > 12)
+        {
+            // reset month value to january
+            tempMonth = (tempMonth-12);
+            // increase year
+            tempYear++;
+        }
+        p=boxMuller(desiredMean,desiredVar);
+        r=margin+p;
+        periodicInterestRate=r/(12/frequency);
+        openingBalance = closingBalance;
+        interest = openingBalance*periodicInterestRate;
+        principal=installment-interest;
+
+        closingBalance = openingBalance-principal;
+    }
+}
 
 #endif //CASECOMBINE_FINALFORMULAS_H
+
+
+
+
